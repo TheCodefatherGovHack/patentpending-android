7e3d29cc6def4222b0bb92ab5ba986f547f53996
Andrew Kevin Gandasugita <andrew.kevin@terem.com.au>
2017-07-29 14:48:32 +1000

    Init

---
+++ app/src/main/java/govhack/thecodefather/patentpending/presentation/MainActivity.java
@@ -0,0 +1,14 @@
+package govhack.thecodefather.patentpending.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import govhack.thecodefather.patentpending.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
