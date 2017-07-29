e3cfc9dc0f155e85a4e7c24c9ae99f1b39826d98
Alberto Camillo <alberto.camillo@terem.com.au>
2017-07-29 18:17:49 +1000

    Updated RecyclerViewSearchAdapter

--- app/src/main/java/govhack/thecodefather/patentpending/presentation/MainActivity.java
+++ app/src/main/java/govhack/thecodefather/patentpending/presentation/MainActivity.java
@@ -1,10 +1,34 @@
 package govhack.thecodefather.patentpending.presentation;
 
 import android.support.v7.app.AppCompatActivity;
-import govhack.thecodefather.patentpending.R;
+import android.support.v7.widget.RecyclerView;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import govhack.thecodefather.patentpending.R;
+import govhack.thecodefather.patentpending.data.models.MessageDataModel;
+import govhack.thecodefather.patentpending.presentation.adapter.RecyclerViewSearchAdapter;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerViewSearchAdapter rvSearchAdapter;
+
+    @ViewById
+    RecyclerView rvSearchResults;
+
+    @AfterViews
+    void initMessagesList() {
+
+        List<MessageDataModel> messages = new ArrayList<>();
+
+        rvSearchAdapter = new RecyclerViewSearchAdapter(messages);
+        rvSearchResults.setAdapter(rvSearchAdapter);
+    }
 
 }
--- app/src/main/java/govhack/thecodefather/patentpending/presentation/adapter/RecyclerViewSearchAdapter.java
+++ app/src/main/java/govhack/thecodefather/patentpending/presentation/adapter/RecyclerViewSearchAdapter.java
@@ -1,12 +1,16 @@
-package govhack.thecodefather.patentpending.adapter;
+package govhack.thecodefather.patentpending.presentation.adapter;
 
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
+
 import govhack.thecodefather.patentpending.R;
+import govhack.thecodefather.patentpending.data.models.MessageDataModel;
 
 /**
  * Created by Alberto Camillo on 29/7/17.
@@ -14,19 +18,33 @@ import govhack.thecodefather.patentpending.R;
 
 public class RecyclerViewSearchAdapter extends RecyclerView.Adapter<RecyclerViewSearchAdapter.PatentViewHolder> {
 
+    List<MessageDataModel> messages;
+
+    public RecyclerViewSearchAdapter(List<MessageDataModel> messages) {
+        this.messages = messages;
+    }
+
     @Override
     public PatentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_patent_item, parent, false);
+        PatentViewHolder pvh = new PatentViewHolder(v);
+        return pvh;
     }
 
     @Override
     public void onBindViewHolder(PatentViewHolder holder, int position) {
-
+        holder.tvPatentTitle.setText(messages.get(position).getTitle());
+        holder.tvPatentStatus.setText(messages.get(position).getType());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return messages.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
     }
 
     public static class PatentViewHolder extends RecyclerView.ViewHolder {
