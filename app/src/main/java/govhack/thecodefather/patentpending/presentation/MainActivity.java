fccec876dea161292b60fbb8982136b0b555cc0e
Andrew Kevin Gandasugita <andrew.kevin@terem.com.au>
2017-07-29 18:10:39 +1000

    add andoird annotation main activity to manifest

--- app/src/main/java/govhack/thecodefather/patentpending/presentation/MainActivity.java
+++ app/src/main/java/govhack/thecodefather/patentpending/presentation/MainActivity.java
@@ -3,7 +3,9 @@ package govhack.thecodefather.patentpending.presentation;
 import android.support.v7.app.AppCompatActivity;
 import govhack.thecodefather.patentpending.R;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Fullscreen;
 
+@Fullscreen
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
