41c9ca752f302e925aa301f3a08f820f6bcaca9c
Alberto Camillo <alberto.camillo@terem.com.au>
2017-07-29 17:49:48 +1000

    Updating RecyclerView and Cardview for search section.

--- app/src/main/java/govhack/thecodefather/patentpending/presentation/MainActivity.java
+++ app/src/main/java/govhack/thecodefather/patentpending/presentation/MainActivity.java
@@ -1,14 +1,17 @@
 package govhack.thecodefather.patentpending.presentation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
 import govhack.thecodefather.patentpending.R;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView rv = (RecyclerView) findViewById(R.id.rvSearchResults);
+    }
 }
