fdf758e4975c43c3e695aeb0745ab2257969a6a8
Alberto Camillo <alberto.camillo@terem.com.au>
2017-07-30 10:19:18 +1000

    Added logic to center status card

--- app/src/main/java/govhack/thecodefather/patentpending/presentation/PatentDetailActivity.java
+++ app/src/main/java/govhack/thecodefather/patentpending/presentation/PatentDetailActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.google.gson.Gson;
 
@@ -41,5 +43,39 @@ public class PatentDetailActivity extends AppCompatActivity {
         horizontalStagesAdapter = new HorizontalStagesAdapter(mSelectedPatent.getStages());
         rvPatentStatuses.setAdapter(horizontalStagesAdapter);
         rvPatentStatuses.setLayoutManager(horizontalLayoutManagaer);
+
+        LinearSnapHelper snapHelper = new LinearSnapHelper() {
+            @Override
+            public int findTargetSnapPosition(RecyclerView.LayoutManager layoutManager, int velocityX, int velocityY) {
+                View centerView = findSnapView(layoutManager);
+                if (centerView == null)
+                    return RecyclerView.NO_POSITION;
+
+                int position = layoutManager.getPosition(centerView);
+                int targetPosition = -1;
+                if (layoutManager.canScrollHorizontally()) {
+                    if (velocityX < 0) {
+                        targetPosition = position - 1;
+                    } else {
+                        targetPosition = position + 1;
+                    }
+                }
+
+                if (layoutManager.canScrollVertically()) {
+                    if (velocityY < 0) {
+                        targetPosition = position - 1;
+                    } else {
+                        targetPosition = position + 1;
+                    }
+                }
+
+                final int firstItem = 0;
+                final int lastItem = layoutManager.getItemCount() - 1;
+                targetPosition = Math.min(lastItem, Math.max(targetPosition, firstItem));
+                return targetPosition;
+            }
+        };
+        snapHelper.attachToRecyclerView(rvPatentStatuses);
+
     }
 }
